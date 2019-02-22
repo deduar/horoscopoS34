c7315d348b7ea7e04d653a97027a940fb63351f5
deduar <deduar@110918>
2019-02-21 23:48:43 -0400

    New Template

--- src/MainBundle/Controller/HoroscopoController.php
+++ src/MainBundle/Controller/HoroscopoController.php
@@ -8,11 +8,75 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class HoroscopoController extends Controller
 {
 	/**
-     * @Route("/", name="horoscopo_index")
+     * @Route("/", name="index")
      */
 	public function indexAction()
 	{
 		return $this->render('horoscopo/index.html.twig', 
 			['controller_name' => 'HoroscopoController',]);
 	}
+
+	/**
+     * @Route("/singin", name="singin")
+     */
+	public function singinAction()
+	{
+		return $this->render('horoscopo/singin.html.twig', 
+			['controller_name' => 'HoroscopoController',]);
+	}
+
+	/**
+     * @Route("/astrologica", name="astrologica")
+     */
+	public function astrologicaAction()
+	{
+		return $this->render('horoscopo/astrologica.html.twig', 
+			['controller_name' => 'HoroscopoController',]);
+	}
+
+	/**
+     * @Route("/signo", name="signo")
+     */
+	public function signoAction()
+	{
+		return $this->render('horoscopo/signo.html.twig', 
+			['controller_name' => 'HoroscopoController',]);
+	}
+
+	/**
+     * @Route("/clarividencia", name="clarividencia")
+     */
+	public function clarividenciaAction()
+	{
+		return $this->render('horoscopo/clarividencia.html.twig', 
+			['controller_name' => 'HoroscopoController',]);
+	}
+
+	/**
+     * @Route("/sexo", name="sexo")
+     */
+	public function sexoAction()
+	{
+		return $this->render('horoscopo/sexo.html.twig', 
+			['controller_name' => 'HoroscopoController',]);
+	}
+
+	/**
+     * @Route("/posiciones", name="posiciones")
+     */
+	public function posicionesAction()
+	{
+		return $this->render('horoscopo/posiciones.html.twig', 
+			['controller_name' => 'HoroscopoController',]);
+	}
+
+	/**
+     * @Route("/forma", name="forma")
+     */
+	public function formaAction()
+	{
+		return $this->render('horoscopo/forma.html.twig', 
+			['controller_name' => 'HoroscopoController',]);
+	}
+
 }
